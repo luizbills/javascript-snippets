dd67c3b3142a348d814684e6ccc58ef88ff16018
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-08-22 13:38:25 -0300

    Update apply-mask.js

--- snippets/apply-mask.js
+++ snippets/apply-mask.js
@@ -35,6 +35,7 @@ console.log(applyMask(162000, 'XX:XX:XX')); // => 16:20:00
 
 // Price
 console.log(applyMask(29, 'R$ XX,00')); // => R$ 29,00
+
 // Example with text input as "date input"
 var input = document.getElementById('my-input');
 input.addEventListener('keyup', function(evt) {
