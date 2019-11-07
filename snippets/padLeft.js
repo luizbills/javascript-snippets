6b367ff34ae0186e0e1467dc28df58aec6c243fe
Luiz Paulo "Bills <luizbills@pm.me>
2019-11-07 12:06:35 -0300

    Update padLeft.js

--- snippets/padLeft.js
+++ snippets/padLeft.js
@@ -1,6 +1,9 @@
 const padLeft = (value, n, char) => Array(n-String(value).length+1).join(char || '0') + value
 
 // old JS
-var padLeft = function padLeft (value, n, char) {
+function padLeft (value, n, char) {
   return Array(n - String(value).length + 1).join(char || '0') + value;
 };
+
+// usage
+console.log(padLeft(7,3,'0')) // => 007
