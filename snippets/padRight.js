132eca2a9917edcd3f8ac1055f8a095277de6074
Luiz Paulo "Bills <luizbills@pm.me>
2019-11-07 12:07:39 -0300

    Update padRight.js

--- snippets/padRight.js
+++ snippets/padRight.js
@@ -4,3 +4,6 @@ const padRight = (value, n, char) => value + Array(n-String(value).length+1).joi
 function padRight (value, n, char) {
   return value + Array(n - String(value).length + 1).join(char || '0');
 };
+
+// usage
+console.log(padRight(8,4,'0')) // => 8000
