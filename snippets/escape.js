3a09b9e71da8369e029a5402b06e80663a0b4369
Luiz Paulo "Bills <luizbills@pm.me>
2019-12-13 16:57:14 -0300

    Create escape.js

---
+++ snippets/escape.js
@@ -0,0 +1,19 @@
+export const escapeHTML = (str) => {
+	const entityMap = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#39;',
+		'/': '&#x2F;',
+		'`': '&#x60;',
+		'=': '&#x3D;'
+	};
+
+	return ('' + str).replace(/[&<>"'`=\/]/g, function (s) {
+		return entityMap[s];
+	});
+}
+
+## usage
+escapeHTML('<div></div>'); // => "&lt;div&gt;&lt;&#x2F;div&gt;"
