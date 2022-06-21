3b864fe2dec1c0c5e0aef68e36b8ab03c440e481
Luiz "Bills <luizbills@pm.me>
2022-06-21 13:45:05 -0300

    Update render-template.js

--- snippets/render-template.js
+++ snippets/render-template.js
@@ -1,5 +1,5 @@
 function renderTemplate(template, data) {
-	let brackets = [ '{{', '}}' ];
+    let brackets = [ '{{', '}}' ];
     let result = template;
     for (const key in data) {
         if (data.hasOwnProperty(key)) {
@@ -10,11 +10,10 @@ function renderTemplate(template, data) {
     return result;
 }
 
-// Note: 
+// Note:
 // a more robust and yet lightweight template engine: https://github.com/jasonmoo/t.js
 
 // Usage
-
 var template = 'hello {{ name }}!'
 var result = renderTemplate(template, {
     name: 'John'
