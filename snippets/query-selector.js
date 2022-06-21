8dd76b72984939d388de79902e98eee6f237c523
Luiz "Bills <luizbills@pm.me>
2022-06-21 13:47:33 -0300

    Update query-selector.js

--- snippets/query-selector.js
+++ snippets/query-selector.js
@@ -1,14 +1,10 @@
 function $ (selector, el = null) {
-    if (!el) {
-        el = document;
-    }
+    if (!el) el = document;
     return el.querySelector(selector);
 }
 
 function $$ (selector, el = null, returnArray = true) {
-    if (!el) {
-        el = document;
-    }
+    if (!el) el = document;
     const res = el.querySelectorAll(selector);
     return returnArray ? Array.from(res) : res; 
 }
