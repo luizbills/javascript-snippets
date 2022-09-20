f57d027259beca4d94e13ff80c30209f011a0fdb
Luiz Bills <luizbills@pm.me>
2022-09-20 16:33:25 -0300

    Create mini-jquery.js

--- snippets/mini-jquery.js
+++ snippets/mini-jquery.js
@@ -1,2 +1,7 @@
 const $ = (s, root = document) => root.querySelector(s);
 const on = (el, evt, cb) => el.addEventListener(evt, cb);
+export const delegate = (el, selector, evt, cb) => {
+    on(el, evt, e => {
+        if (e.target.matches(selector)) cb(e, el);
+    });
+};
