f66cceba709fac746bf6604106d728c39d7e6cd7
Luiz Bills <luizbills@pm.me>
2023-07-27 11:16:31 -0300

    Update mini-jquery.js

--- snippets/mini-jquery.js
+++ snippets/mini-jquery.js
@@ -1,5 +1,6 @@
 const $ = (s, root = document) => root.querySelector(s);
 const on = (el, evt, cb) => el.addEventListener(evt, cb);
+const trigger = (el, evt, detail = null) => el.dispatchEvent(new CustomEvent(evt, { detail }));
 const delegate = (el, selector, evt, cb) => {
     on(el, evt, e => {
         if (e.target.matches(selector)) cb(e, el);
